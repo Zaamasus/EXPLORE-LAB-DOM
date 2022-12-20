ae5d1d0f09769108c8d245d4b5e83194d4a85b22
Zaamasus <samuelgomes22442@gmail.com>
2022-12-20 14:05:01 -0300

    teste

--- index-syper.js
+++ index-syper.js
@@ -27,21 +27,22 @@ const swiper = new Swiper('.swiper', {
 });
 
 /*  abre e fecha o menu quando clicar no icone: hamburguer e x */
-// const nav = document.querySelector('#header nav')
-// console.log(toggle)
-// const toggle = document.querySelectorAll('nav .toggle img')
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-// for (const element of toggle) {
-//   element.addEventListener('click', function () {
-//     nav.classList.toggle('show')
-//   })
-// }
 
-// /* quando clicar em um item do menu, esconder o menu */
-// const links = document.querySelectorAll('nav ul li a')
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
 
-// for (const link of links) {
-//   link.addEventListener('click', function () {
-//     nav.classList.remove('show')
-//   })
-// }
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
